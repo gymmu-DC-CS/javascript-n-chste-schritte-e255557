9a1944e0573b876a1653869711bbe85b340580ae
Lorella <e255557@edu.sbl.ch>
2023-11-21 10:25:20 +0100

    aufgabe 2 angefangen

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ export function aufgabe01(args) {
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
     if (currentElement !="e") {
+    if (currentElement !="E") {
     result.push(currentElement)
     } 
 
@@ -12,3 +13,22 @@ export function aufgabe01(args) {
   return result.join("")
 }
 
+export function aufgabe02(args) {
+  const input = args
+  const result = []
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === "a") {
+      result.push("A")
+    } else if (currentElement === "b") {
+      result.push ("B")
+    } else if (currentElement === "c") {
+      result.push ("C")
+    }else {
+      result.push(currentElement)
+      }
+      }
+      return result.join("")
+  }
+  
